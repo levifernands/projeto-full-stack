27a10746245cf7c96749c905a340eaefcff2981b
Levi Fernandes <levifernands31@gmail.com>
2025-02-14 18:41:58 -0300

    feat: criar modulo de tarefas

--- server-api/src/app.module.ts
+++ server-api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './app/tasks/tasks.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    TasksModule,
   ],
   controllers: [],
   providers: []
