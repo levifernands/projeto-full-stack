92e2ac0184fc78166a5ea6835409709ab22434ac
Levi Fernandes <levifernands31@gmail.com>
2025-02-14 18:28:02 -0300

    chore: adicionar credenciais em arquivo env

--- server-api/src/app.module.ts
+++ server-api/src/app.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'tarefas-db',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3306)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', 'root'),
+        database: configService.get('DB_NAME', 'tarefas-db'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
+  controllers: [],
+  providers: []
 })
-export class AppModule {}
+export class AppModule { }
