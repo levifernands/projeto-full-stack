60b65719a67141ac0afd5b2f926ebd2478f00888
Levi Fernandes <levifernands31@gmail.com>
2025-02-14 22:27:36 -0300

    feat: create FindTaskById endpoint

--- server-api/src/app/tasks/tasks.service.ts
+++ server-api/src/app/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskEntity } from './entity/task.entity';
 import { Repository } from 'typeorm';
@@ -14,7 +14,14 @@ export class TasksService {
     return await this.taskRepository.find();
   }
 
-  async findTaskById() { }
+  async findTaskById(id: string): Promise<TaskEntity> {
+    try {
+      return await this.taskRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      console.error('Erro ao procurar tarefa por ID:', error);
+      throw new NotFoundException(`Tarefa com ID ${id} não encontrada.`);
+    }
+  }
 
   async createTask() { }
 
