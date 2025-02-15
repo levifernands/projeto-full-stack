18546eba16023a530053c7d9ff3a765a663272e2
Levi Fernandes <levifernands31@gmail.com>
2025-02-14 22:12:33 -0300

    feat: create FindAllTasks endpoint

--- server-api/src/app/tasks/tasks.service.ts
+++ server-api/src/app/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskEntity } from './entity/task.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TasksService { }
+export class TasksService {
+  constructor(
+    @InjectRepository(TaskEntity)
+    private readonly taskRepository: Repository<TaskEntity>,
+  ) { }
+
+  async findAllTasks(): Promise<TaskEntity[]> {
+    return await this.taskRepository.find();
+  }
+
+  async findTaskById() { }
+
+  async createTask() { }
+
+  async updateTaskById() { }
+
+  async deleteTaskById() { }
+}
