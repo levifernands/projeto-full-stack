aab145be3238ff8458b1237d1e0e6370a08542c0
Levi Fernandes <levifernands31@gmail.com>
2025-02-16 00:23:29 -0300

    feat: criar endpoint de serviço para UpdateUser

--- server-api/src/app/users/users.service.ts
+++ server-api/src/app/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { FindOneOptions, FindOptionsWhere, Repository } from 'typeorm';
@@ -23,7 +23,23 @@ export class UsersService {
       if (error.name === 'EntityNotFound') {
         throw new NotFoundException(`Usuário não encontrado nessas condições: ${JSON.stringify(conditions)}. Erro: ${error.message}`);
       }
-      throw new NotFoundException(`An unexpected error occurred: ${error.message}`);
+      throw new NotFoundException(`Erro ao buscar usuário: ${error.message}`);
+    }
+  }
+
+  async updateUser(id: string, userData): Promise<UserEntity> {
+    try {
+      const user = await this.findOneOrFail({ id });
+
+      if (!user) {
+        throw new NotFoundException(`Usuário com ID ${id} não encontrado.`);
+      }
+
+      this.userRepository.merge(user, userData);
+      return await this.userRepository.save(user);
+
+    } catch (error) {
+      throw new InternalServerErrorException('Erro ao atualizar o usuário.', error);
     }
   }
 }
