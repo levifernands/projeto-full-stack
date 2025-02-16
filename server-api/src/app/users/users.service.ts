d1366e3982c3cd27018871754758444b47a29749
Levi Fernandes <levifernands31@gmail.com>
2025-02-15 23:54:08 -0300

    feat: criar endpoint de serviço para FindAllUsers

--- server-api/src/app/users/users.service.ts
+++ server-api/src/app/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>,
+  ) { }
+
+  async findAllUsers(): Promise<UserEntity[]> {
+    return await this.usersRepository.find({
+      select: ['id', 'firstName', 'lastName', 'email']
+    })
+  }
+}
