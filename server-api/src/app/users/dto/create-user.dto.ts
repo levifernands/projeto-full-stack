0c6ceaad590b37daf34d06010c3c3af82dfab65c
Levi Fernandes <levifernands31@gmail.com>
2025-02-16 18:37:53 -0300

    feat: documentar apis de users

--- server-api/src/app/users/dto/create-user.dto.ts
+++ server-api/src/app/users/dto/create-user.dto.ts
@@ -1,24 +1,41 @@
 import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 import { PasswordValidationHelper } from '../helpers/passwordValidation.helper';
 import { CustomMessagesHelper } from '../helpers/customMessages.helper';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: 'Primeiro nome do usuário',
+    example: 'João',
+  })
   firstName: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: 'Último nome do usuário',
+    example: 'Silva',
+  })
   lastName: string;
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({
+    description: 'E-mail do usuário',
+    example: 'joao.silva@email.com',
+  })
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @Matches(PasswordValidationHelper.password, {
     message: CustomMessagesHelper.PASSWORD_RULE,
+  })
+  @ApiProperty({
+    description: 'Senha do usuário (deve seguir regras de complexidade)',
+    example: 'Senha@123',
   })
   password: string;
 }
