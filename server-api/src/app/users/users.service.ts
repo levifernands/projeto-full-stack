71c8f312b23ca7659f5c7d818075990a0548c400
Levi Fernandes <levifernands31@gmail.com>
2025-02-16 00:10:29 -0300

    feat: criar endpoint de serviço para FindOneOrFail

--- server-api/src/app/users/users.service.ts
+++ server-api/src/app/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import { FindOneOptions, FindOptionsWhere, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
@@ -14,5 +14,16 @@ export class UsersService {
     return await this.userRepository.find({
       select: ['id', 'firstName', 'lastName', 'email']
     })
+  }
+
+  async findOneOrFail(conditions: FindOptionsWhere<UserEntity>, options?: FindOneOptions<UserEntity>) {
+    try {
+      return await this.userRepository.findOneOrFail({ where: conditions, ...options });
+    } catch (error) {
+      if (error.name === 'EntityNotFound') {
+        throw new NotFoundException(`Usuário não encontrado nessas condições: ${JSON.stringify(conditions)}. Erro: ${error.message}`);
+      }
+      throw new NotFoundException(`An unexpected error occurred: ${error.message}`);
+    }
   }
 }
