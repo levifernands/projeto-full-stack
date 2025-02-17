98e518e536734ffebcc357eb1113331de62c4f74
Levi Fernandes <levifernands31@gmail.com>
2025-02-16 23:36:37 -0300

    feat: configurar rotas e criar componente de login (inicialmente mockado)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './styles/App.css'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TaskWrapper } from './components/TaskWrapper';
+import { LoginPage } from "./components/LoginPage";
+
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Minha Lista de Tarefas</h1>
-      <TaskWrapper />
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<TaskWrapper />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  </Router>
   );
 };
 
---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+
+    if (email === "admin@email.com" && password === "123456") {
+      localStorage.setItem("auth", "true"); 
+      navigate("/");
+    } else {
+      alert("Credenciais inválidas");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+};
