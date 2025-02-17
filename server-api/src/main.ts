6789ef7f7ae5e62efe59db5305246aa75a6b0477
Levi Fernandes <levifernands31@gmail.com>
2025-02-17 00:19:52 -0300

    fix: corrigir problema de CORS no backend

--- server-api/src/main.ts
+++ server-api/src/main.ts
@@ -15,6 +15,12 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: `http://${process.env.FRONT_HOST}:${process.env.FRONT_PORT}`,
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
