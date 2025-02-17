d88d10d1b36920947f4e5e088c90bcc83da782c2
Levi Fernandes <levifernands31@gmail.com>
2025-02-17 01:48:16 -0300

    feat: estilizar input

--- frontend/src/components/TaskInput.js
+++ frontend/src/components/TaskInput.js
@@ -5,21 +5,30 @@ export const TaskInput = ({ initialValue = "", onSubmit, placeholder }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (value.trim() === "") return;
+
+    if (value.trim() === "") {
+      alert("O título não pode estar vazio.");
+      return;
+    }
+
+    console.log("Enviando tarefa com título:", value);
+
     onSubmit(value);
-    setValue(""); 
+    setValue("");
   };
 
   return (
     <form onSubmit={handleSubmit} className="TaskInput">
-      <input 
-        type="text" 
-        value={value} 
-        onChange={(e) => setValue(e.target.value)} 
-        placeholder={placeholder} 
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder={placeholder}
         className="task-input"
       />
-      <button type="submit" className="task-botao">Salvar</button>
+      <button type="submit" className="task-botao">
+        Salvar
+      </button>
     </form>
   );
 };
